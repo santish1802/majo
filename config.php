c19c79a738300b9b8d22d69fa391b62108b2d936
santish1802 <santiago497045@gmail.com>
2025-10-28 01:33:15 -0500

    config

--- config.php
+++ config.php
@@ -1,12 +1,25 @@
 <?php
 date_default_timezone_set('America/Lima'); // Establece la zona horaria a Lima, Perú
 
-
+if ($_SERVER['HTTP_HOST'] === 'majocafe.site') {
+    // Producción (UltraHost)
+    $host     = 'localhost';
+    $db       = 'fdrcsdqs_majocafe_system';
+    $user     = 'fdrcsdqs_santiago';
+    $pass     = 'P75609967p';
+} elseif ($_SERVER['HTTP_HOST'] === 'majo.kesug.com') {
+    // Producción (InfinityFree)
+    $host     = 'sql113.infinityfree.com';
+    $db       = 'if0_39951106_dulces_antojos';
+    $user     = 'if0_39951106';
+    $pass     = 'Maras220124';
+} else {
+    // Localhost
     $host     = 'localhost';
     $db       = 'majocafe_system';
     $user     = 'root';
     $pass     = '';
-
+}
 
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
